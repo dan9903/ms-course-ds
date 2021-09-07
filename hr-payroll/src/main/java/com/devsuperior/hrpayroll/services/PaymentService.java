787b9b96ae086291fb2d5a4c3dc086f363a6cfd3
Damian Meneses <mnssd2@gmail.com>
2021-09-06 21:40:42 -0300

    implementation of feign client hr-payroll

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,31 +1,21 @@
 package com.devsuperior.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feingclients.WorkerFeingClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-  @Value("${hr-worker.host}")
-  private String hrWorkerHost;
-
   @Autowired
-  private RestTemplate restTemplate;
+  private WorkerFeingClient workerFeingClient;
 
   public Payment getPayment(long workerId, int days) {
-    Map<String, String> uriVariables = new HashMap<String, String>();
-    uriVariables.put("id", Long.toString(workerId));
-    String url = hrWorkerHost + "/workers/{id}";
 
-    Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+    Worker worker = workerFeingClient.findById(workerId).getBody();
 
     return new Payment(worker.getName(), worker.getDailyIncome(), days);
   }
